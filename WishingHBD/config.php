ebaff4b762445a63f80116bc5920e62351880604
Cindy-Edy <ngrok496@gmail.com>
2024-11-28 16:47:08 +0700

    Add files via upload

---
+++ WishingHBD/config.php
@@ -0,0 +1,42 @@
+<?php error_reporting(0); $name = $_GET['name']; 
+
+$page_name = $name."Special surprise gift"; 
+
+$finger = "1"; // 1 = on  ****** 0 = off
+
+$text_finger1 = "You have a special surprise gift";
+$text_finger2 = "Scan your finger to view your surprising gift.";
+
+$sharing_url = "https://website.com";
+
+$sharing_message = $name." You have a special surprise gift Here"; 
+
+$sharing_invite = "Click here to view your big surprising gift.";
+
+$sharing_advice = "Just a big surprise gift here!";
+
+$sharing_name = "Click here to view your big surprising gift."; 
+
+$sharing_description = "Just a big surprise gift here!";
+
+$happy_from = "HAPPY BIRTHDAY";
+
+$happy_message = "All the best wishes";
+
+$aid_creat = "You can create your own greeting message by entering the name in the box bellow and clicking the Create button";
+
+$ads_validation = "ca-pub-XXXXXXXXXXXXXXXX";
+
+$small_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
+                
+$small_ads_slot = "XXXXXXXXXX";
+          
+$big_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
+
+$big_ads_slot = "XXXXXXXXXX";
+
+$tracking = "UA-XXXXXXXXX-1";
+
+$fb_app_id = "XXXXXXXXXXXXXXXX";
+
+?>
